a92252841f644de7a5dac020eeff070d0f3529cf
tomatine <tomatine86@gmail.com>
2020-09-15 01:05:47 +0900

    一通り完成 discordbotへのタイマー機能追加、set_timerの関数化、コメント追加

--- discord_homebot.py
+++ discord_homebot.py
@@ -3,9 +3,13 @@
 
 import discord
 import os
+import re
 import logging
+from mojimoji import zen_to_han#半角を全角に変換するメソッド
 from send_ir import fetch_code, send
+from set_timer import timer
 
+#ログ取り用スニペットのコピペ（discord.logに出力）
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -13,7 +17,30 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(mes
 logger.addHandler(handler)
 
 
-client=discord.Client()
+client = discord.Client()
+#クーラーオン、ヒーターオン、エアコンオフなどの、赤外線コードの入ったファイル名を変数指定
+cooler_on_filename='cooler_on_25.txt'
+heater_on_filename = 'heater_on_22.txt'
+aircon_off_filename = 'aircon_stop.txt'
+light_on_filename = 'light_on.txt'
+light_off_filename = 'light_off.txt'
+light_night_filename='light_night.txt'#常夜灯
+
+
+#タイマーセット用関数（function_nameは暖房、冷房、オフなど、タイマーの名前）
+async def timer_conversation(message, filename, function_name: str):
+    try:
+        time=re.findall('[0-9]+', zen_to_han(message.content))
+        hour = int(time[0])
+        minute = int(time[1])
+        if hour < 0 or hour > 23 or minute < 0 or minute > 59:
+            raise IndexError
+        else:
+            timer(hour,minute,filename)
+            await message.channel.send('{0}:{1}に'.format(hour, minute)+function_name+'タイマーをセットしました！')
+    except (IndexError):
+        await message.channel.send('正しい形式の時刻を入力して！')
+
 
 @client.event
 async def on_ready():
@@ -26,9 +53,12 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content == 'ヘルプ':
-        help_message = '冷房オン---25度で冷房をつけます\n\
-暖房オン---暖房をつけます\n\
+    if message.content.startswith('ヘルプ'):
+        help_message ='ライトオン---ライトを点けます\n\
+ライトオフ---ライトを消します\n\
+常夜灯---常夜灯にします\n\
+冷房オン---25度で冷房をつけます\n\
+暖房オン---22度で暖房をつけます\n\
 エアコンオフ---エアコンを止めます\n\
 冷房タイマー22:30---22:30に冷房をつけます\n\
 暖房タイマー22:30---22:30に暖房をつけます\n\
@@ -40,21 +70,47 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('nyan')
 
+    if message.content == 'ありがとう':
+        await message.channel.send('どういたしまして😁')
+
     if message.content == '冷房オン':
-        code = fetch_code('cooler_on_25.txt')
+        code = fetch_code(cooler_on_filename)
         send(code)
         await message.channel.send('冷房をつけました！')
 
     if message.content == '暖房オン':
+        code = fetch_code(heater_on_filename)
+        send(code)
         await message.channel.send('暖房をつけました！')
 
     if message.content == 'エアコンオフ':
-        code = fetch_code('aircon_stop.txt')
+        code = fetch_code(aircon_off_filename)
         send(code)
         await message.channel.send('エアコンを止めました！')
 
-    if message.content.startswith('冷房タイマー'):
-        
-        await message.channel.send('{0}:{1}にタイマーをセットしました！'.format(hour,minute))
+    if message.content == 'ライトオン':
+        code = fetch_code(light_on_filename)
+        send(code)
+        await message.channel.send('ライトを点けました！')
 
+    if message.content == 'ライトオフ':
+        code = fetch_code(light_off_filename)
+        send(code)
+        await message.channel.send('ライトを消しました！')
+    
+    if message.content == '常夜灯':
+        code = fetch_code(light_night_filename)
+        send(code)
+        await message.channel.send('常夜灯にしました！')
+
+    if message.content.startswith('冷房タイマー'):
+        await timer_conversation(message, cooler_on_filename, '冷房')
+
+    if message.content.startswith('暖房タイマー'):
+        await timer_conversation(message, heater_on_filename, '暖房')
+
+    if message.content.startswith('オフタイマー'):
+        await timer_conversation(message, aircon_off_filename, 'オフ')
+
+#環境変数からdiscordbotのトークンを取得
 client.run(os.environ.get("discord_homebot_token"))
--- set_timer.py
+++ set_timer.py
@@ -3,23 +3,20 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    argv = sys.argv
+def timer(hour: int, minute: int, filename):
     try:
         crontab_setting_file='crontab-data'#crontab設定ファイル名
-        #hourとminuteが24、60に収まっているか確認追記すること
-        #現状クーラーオンのみなので、選択できるようにすること
-        hour = argv[1]
-        minute=argv[2]
-        crontab_setting = minute + " " + hour + " * * * python3 send_ir.py cooler_on_25.txt\n" \
-            "crontab -r\n"
-        #crontab設定ファイル書き換え
+        crontab_setting = str(minute) + " " + str(hour) + " * * * python3 send_ir.py "+filename+";crontab -r\n"  #crontab設定ファイル書き換え
         with open(crontab_setting_file, 'w') as f:
             f.write(crontab_setting)
         #crontab設定
-        os.system('crontab crontab-data')#windows上で試す際はコメントアウトすること（エラー出る）
-        sys.exit(0)
+        os.system('crontab crontab-data')
 
     except (OSError, IndexError):
-        sys.stderr.write('Use with arguments hh mm\n(example:\'set_timer.py 22 50\' for set timer in 22:30)\n')
+        sys.stderr.write('Use with arguments hh mm filename\n(example:\'set_timer.py 22 50 cooler_on_25.txt\' for set cooler on in 22:50)\n')
         sys.exit(-1)
+
+
+if __name__ == '__main__':
+    argv = sys.argv
+    timer(argv[1],argv[2],argv[3])
