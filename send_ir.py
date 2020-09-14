79843af882ae32a3cd495f951d84ef2e719a28e7
tomatine <tomatine86@gmail.com>
2020-09-14 15:50:19 +0900

    赤外線コード取得関数化 discordbotでimportするため

--- send_ir.py
+++ send_ir.py
@@ -6,19 +6,25 @@ import sys
 def send(code):
     os.system('/usr/local/bin/bto_advanced_USBIR_cmd -d ' + code)
 
-if __name__ == '__main__':
-    argv = sys.argv
+def fetch_code(filename):
     try:
-        filename = argv[1]
         if os.path.isfile(filename) is True:
             f = open(filename, 'r')
             code = f.read()
-            send(code)
-            sys.exit(0)
+            return code
         else:
-           sys.stderr.write('File not found.\n')
-           sys.exit(-1)
+            sys.stderr.write('File not found.\n')
+            sys.exit(-1)
+    
+    except (OSError, IndexError):
+        sys.stderr.write('File not found.\n')
+        sys.exit(-1)
 
+if __name__ == '__main__':
+    try:
+        argv = sys.argv
+        code = fetch_code(argv[1])
+        send(code)
     except (OSError, IndexError):
         sys.stderr.write('File not found.\n')
         sys.exit(-1)
