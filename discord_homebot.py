51acb7ce69fdcdf91def9ffb738838639b7ec763
tomatine <tomatine86@gmail.com>
2020-09-14 16:44:18 +0900

    discorbot機能追加、.gitignore追記 discordbotにエアコン操作機能追加、webhook経由のボットに反応するよう書き換え .gitignoreにcache追加

--- discord_homebot.py
+++ discord_homebot.py
@@ -3,6 +3,7 @@
 
 import discord
 import os
+from send_ir import fetch_code, send
 
 client=discord.Client()
 
@@ -12,19 +13,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
+    if message.author == client.user:
         return
     
-    if message.content=='/neko':
+    if message.content == 'ヘルプ':
+        help_message='\n冷房オン：25度で冷房をつけます\n暖房オン：暖房をつけます\nエアコンオフ：エアコンを止めます\n'
+        await message.channel.send(help_message)
+
+    if message.content == '/neko':
         await message.channel.send('nyan')
 
     if message.content == '冷房オン':
+        code = fetch_code('cooler_on_25.txt')
+        send(code)
         await message.channel.send('冷房をつけました！')
 
     if message.content == '暖房オン':
         await message.channel.send('暖房をつけました！')
 
     if message.content == 'エアコンオフ':
+        code = fetch_code('aircon_stop.txt')
+        send(code)
         await message.channel.send('エアコンを止めました！')
 
 
